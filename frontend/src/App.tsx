5443b3eb6bae4b0b0f80e0fb59bdd3fe6e6ece3d
Jamily Batista <123518956+JamilyB@users.noreply.github.com>
2026-08-24 10:56:24 -0300

    Refactor routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,74 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { ProfilePage } from "./modules/cliente/pages/ProfilePage";
 import RegisterPage from "./modules/cliente/pages/RegisterPage";
+
 import Header from "./shared/components/Header";
 import Footer from "./shared/components/Footer";
+
 import CatalogPage from "./modules/produtos/pages/CatalogPage";
+
 import CheckoutPage from "./modules/vendas/pages/CheckoutPage";
+import OrdersPage from "./modules/vendas/pages/OrdersPage";
+import ReturnsPage from "./modules/vendas/pages/ReturnsPage";
+
 import ProductsPage from "./modules/adm/pages/ProductsPage";
-import InventoryPage from "./modules/adm/pages/InventoryPage";
 import DashboardPage from "./modules/adm/pages/DashboardPage";
 import OrdersPageADM from "./modules/adm/pages/OrdersPage";
-import ReturnsPageADM from "./modules/adm/pages/ReturnsPage"
-import CustomerPage from "./modules/adm/pages/CustomerPage"
-import OrdersPage from "./modules/vendas/pages/OrdersPage"
-import ReturnsPage from "./modules/vendas/pages/ReturnsPage"
+import ReturnsPageADM from "./modules/adm/pages/ReturnsPage";
+import CustomerPage from "./modules/adm/pages/CustomerPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/clientes" element={<CustomerPage />} />
-        <Route path="/produtos" element={<ProductsPage />} />
-        <Route path="/catalogo" element={<CatalogPage />} />
-        <Route path="/comprar" element={<CheckoutPage />} />
-        <Route path="/pedidos" element={<OrdersPage />} />
-        <Route path="/devolucoes" element={<ReturnsPage />} />
-        <Route path="/inventario" element={<InventoryPage />} /> 
-        <Route path="/perfil" element={<ProfilePage />} />
+        <Route path="/catalogo" element={
+            <>
+              <Header />
+              <CatalogPage />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/comprar" element={
+            <>
+              <Header />
+              <CheckoutPage />
+              <Footer />
+            </>
+          }
+        />
+
+        <Route path="/pedidos" element={
+            <>
+              <Header />
+              <OrdersPage />
+              <Footer />
+            </>
+          }
+        />
+
+        <Route path="/devolucoes" element={
+            <>
+              <Header />
+              <ReturnsPage />
+              <Footer />
+            </>
+          }
+        />
+
+        <Route path="/perfil" element={
+            <>
+              <Header />
+              <ProfilePage />
+              <Footer />
+            </>
+          }
+        />
         <Route path="/cadastrar" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/produtos" element={<ProductsPage />} />
+        <Route path="/clientes" element={<CustomerPage />} />
         <Route path="/vendas" element={<OrdersPageADM />} />
         <Route path="/adm/devolucoes" element={<ReturnsPageADM />} />
       </Routes>
